1099f7137f0ee907cec1f2ee8b4da4a0b8d79ae5
Yen Hung Lin <lllyyyhhh45@hotmail.com>
2020-05-27 10:59:14 -0700

    Updates

---
+++ precache-manifest.05eedd8239d467a41c2bd03c06319a58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9bf1cd1288ce8623d382e3456f3486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32879cb63c658e2ae43a",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4dd8d4ac54b3982ac9c3",
+    "url": "/static/css/main.327da345.chunk.css"
+  },
+  {
+    "revision": "32879cb63c658e2ae43a",
+    "url": "/static/js/2.595afa5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.595afa5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd8d4ac54b3982ac9c3",
+    "url": "/static/js/main.b0bcc858.chunk.js"
+  },
+  {
+    "revision": "909a833fac971ede64ec",
+    "url": "/static/js/runtime-main.5ec28338.js"
+  }
+]);
